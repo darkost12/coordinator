0429026f04c3cae41bed9d1e17f61a9b2326bf8d
Konstantin Akhanshchikov <k.akhanshchikov@planado.app>
2023-01-06 21:26:42 +0300

    Handle HTTP requests

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import type { Interaction } from 'discord.js'
 import { IntentsBitField, ActivityType } from 'discord.js'
 import { Client } from 'discordx'
 import { ensureIndexes } from './db.js'
+import { createServer } from 'http'
 
 const bot = new Client({
   intents: [
@@ -34,6 +35,8 @@ async function run() {
     await bot.login(process.env.BOT_TOKEN)
 
     bot.user?.setActivity('your activities', { type: ActivityType.Watching })
+
+    createServer((_, res) => res.end('Bot is alive!')).listen(3000)
   } else {
     throw Error('Could not find BOT_TOKEN')
   }
